911582765d07de24f891ff32ca0a321ad801f1ea
ValentinS83 <valentinstefan77@yahoo.com>
2024-03-04 19:14:17 +0200

    Add additional exap

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ console.log(age);
 age = 36;
 console.log(age);
 
+const piNumber = 3.14;
+console.log(piNumber);
+// valorile din constante nu pot fi schimbate
+
 let userName = 'Valentin';
 console.log('name: ');
 console.log(userName);
+
+// Operatori data numerice
+// adunare
+let firstNumber = 10;
+firstNumber = 12;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
+// string interpolation
 
